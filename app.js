dd17bce4c22e569fc1b56071432294d56e392b3a
Felipe Guarda <fguarda@minsait.com>
2023-04-26 23:41:59 -0300

    pokeDex commit updated

--- app.js
+++ app.js
@@ -19,10 +19,14 @@ const getTypeColor = type => {
   }[type] || normal
 }
 
-const getPokemonsType = async pokeApiResults => {
-  const promises = pokeApiResults.map(result => fetch(result.url))
+const getOnlyfulfilled = async ({ func, arr}) => {
+  const promises = arr.map(func)
   const responses = await Promise.allSettled(promises)
-  const fulfilled = responses.filter(response => response.status === 'fulfilled')
+  return responses.filter(response => response.status === 'fulfilled')
+}
+
+const getPokemonsType = async pokeApiResults => {
+  const fulfilled = await getOnlyfulfilled({ arr: pokeApiResults, func: result => fetch(result.url) })
   const pokePromises = fulfilled.map(url => url.value.json())
   const pokemons = await Promise.all(pokePromises)
   return pokemons.map(fulfilled => fulfilled.types.map(info => info.type.name))
@@ -33,23 +37,53 @@ const getPokemonsIds = pokeApiResults => pokeApiResults.map(({url }) => {
     return urlAsArray.at(urlAsArray.length -2)
   })
 
+const getPokemonsImgs = async ids => {
+    const fulfilled = await getOnlyfulfilled({ arr: ids, func: id => fetch(`./assets/img/${id}.png`) })
+    return fulfilled.map(response => response.value.url)
+}
 
-const handlePageLoaded = async () => {
+const getPokemons = async () => {
   try {
     const response = await fetch ('https://pokeapi.co/api/v2/pokemon?limit=15&offset=0')
 
     if (!response.ok) {
-      throw Error ('Não foi possível obter as informações')
+      throw new Error ('Não foi possível obter as informações')
     }
 
     const { results: pokeApiResults } = await response.json()
     const types = await getPokemonsType(pokeApiResults)
     const ids = getPokemonsIds(pokeApiResults)
+    const imgs = await getPokemonsImgs(ids)
+    const pokemons = ids.map((id, i) => ({ id, name: pokeApiResults[i].name, types: types[i], imgUrl: imgs[i] }))
 
-    console.log(ids)
+    return pokemons
   } catch (error) {
     console.log('algo deu errado: ', error)
   }
+}
+
+const renderPokemons = pokemons => {
+  const ul = document.querySelector('[data-js="pokemons-list"]')
+
+  pokemons.forEach(({ id, name, types, imgUrl}) => {
+    const li = document.createElement('li')
+    const img = document.createElement('img')
+    const nameContainer = document.createElement('h2')
+    const typeContainer = document.createElement('p')
+    const [firstType] = types
+
+    img.setAttribute('src', imgUrl)
+    img.setAttribute('alt', name)
+    li.setAttribute('class', `card ${firstType}`)
+    li.style.setProperty('--type-color', getTypeColor(firstType))
+
+    console.log(li)
+  })
+}
+
+const handlePageLoaded = async () => {
+  const pokemons = await getPokemons()
+  renderPokemons(pokemons)
 }
 
 handlePageLoaded()
